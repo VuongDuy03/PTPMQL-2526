c4c63a131ae8bd64912bbabdc16a105119ccbbb6
VuongDuy03 <vuongduy756@gmail.com>
2025-09-04 15:09:56 +0700

    BTH

--- DemoMVC/Controllers/PersonController.cs
+++ DemoMVC/Controllers/PersonController.cs
@@ -1,7 +1,13 @@
+using System.Net.Mime;
 using Microsoft.AspNetCore.Mvc;
 using DemoMVC.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DemoMVC.Data;
+using Microsoft.EntityFrameworkCore;
 
-namespace DemoMVC.Controllers;
+namespace DemoMVC.Controllers
+{
 
     public class PersonController : Controller
     {
@@ -18,5 +24,21 @@ namespace DemoMVC.Controllers;
             ViewBag.Message = ps.PersonID + "-" + ps.FullName + "-" + ps.Address;
             return View();
         }
+
+
+        private readonly ApplicationDbContext _context;
+        public PersonController(ApplicationDbContext context);
+        public async Task<IActionResult> Index()
+        
     }
 
+    [Table("persons")]
+    public class Person
+    {
+        [Key]
+        public string PersonID { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+    }
+
+}
--- DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c61c18bbbbda50ad4551e150a98b8a747154f0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee885641a553f6b0f78704906049512168e1437c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
